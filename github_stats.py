6991b567d38826d4077ba1915249e3f6218458c5
Mihhail Plohhotnitsenko <36076591+Mix-Anik@users.noreply.github.com>
2026-03-07 06:53:40 +0200

    Fix formatting

--- github_stats.py
+++ github_stats.py
@@ -117,7 +117,7 @@ def get_commits_data() -> int:
 def compose_stats_table(lang_data, header_stats) -> str:
     lines = []
     lines.append("┌────────────────────────────────────┐")
-    lines.append(f"│      📊top{header_stats:>21} │")
+    lines.append(f"│      📊top{header_stats:>20} │")
     lines.append("├─────────────────┬──────────────────┤")
 
     for idx, (lang, sz, pct) in enumerate(lang_data, 1):
